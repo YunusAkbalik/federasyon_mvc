9961509aa2965bda503927990c67e74972e735db
YunusAkbalik <yunusroose@gmail.com>
2023-02-27 15:34:49 +0300

    ogretmen sinif list update

--- resources/views/ogretmen/sinif/list.blade.php
+++ resources/views/ogretmen/sinif/list.blade.php
@@ -1,5 +1,7 @@
 @extends('layouts.backend')
-
+@section('css')
+    <link rel="stylesheet" href="{{ asset('assets/js/plugins/datatables-bs5/css/dataTables.bootstrap5.min.css') }}">
+@endsection
 @section('content')
     <!-- Hero -->
     <div class="bg-body-light">
@@ -16,7 +18,7 @@
         <!-- Your Block -->
         <div class="block block-rounded">
             <div class="block-content">
-                <table class="table">
+                <table class="table table-bordered table-striped table-vcenter js-dataTable-full">
                     <thead>
                         <tr>
                             <th>Sınıf</th>
@@ -28,12 +30,26 @@
                                 <td>{{ $sinif[1] }}</td>
                             </tr>
                         @endforeach
-                    </tbody>    
+                    </tbody>
                 </table>
-               
+
             </div>
         </div>
         <!-- END Your Block -->
     </div>
     <!-- END Page Content -->
+@endsection
+@section('js')
+    <script src="{{ asset('assets/js/plugins/datatables/jquery.dataTables.min.js') }}"></script>
+    <script src="{{ asset('assets/js/plugins/datatables-bs5/js/dataTables.bootstrap5.min.js') }}"></script>
+    <script src="{{ asset('assets/js/plugins/datatables-responsive/js/dataTables.responsive.min.js') }}"></script>
+    <script src="{{ asset('assets/js/plugins/datatables-responsive-bs5/js/responsive.bootstrap5.min.js') }}"></script>
+    <script src="{{ asset('assets/js/plugins/datatables-buttons/dataTables.buttons.min.js') }}"></script>
+    <script src="{{ asset('assets/js/plugins/datatables-buttons-bs5/js/buttons.bootstrap5.min.js') }}"></script>
+    <script src="{{ asset('assets/js/plugins/datatables-buttons-jszip/jszip.min.js') }}"></script>
+    <script src="{{ asset('assets/js/plugins/datatables-buttons-pdfmake/pdfmake.min.js') }}"></script>
+    <script src="{{ asset('assets/js/plugins/datatables-buttons-pdfmake/vfs_fonts.js') }}"></script>
+    <script src="{{ asset('assets/js/plugins/datatables-buttons/buttons.print.min.js') }}"></script>
+    <script src="{{ asset('assets/js/plugins/datatables-buttons/buttons.html5.min.js') }}"></script>
+    <script src="{{ asset('assets/js/pages/ogretmenSinifList.js') }}"></script>
 @endsection
